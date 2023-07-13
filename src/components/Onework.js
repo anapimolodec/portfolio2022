a7f7bdea542fc197326ab9c36cb35b4cee33ccb3
Aiaru <aiaru.mukhamedyarova@gmail.com>
2023-07-13 15:36:53 +0900

    changed layout of works, erased old works

--- src/components/Onework.js
+++ src/components/Onework.js
@@ -64,6 +64,11 @@ const buttonStyle2 = {
   color: 'black',
 
 }
+
+const imgSvg = {
+  width: '40%',
+  padding: '10px',
+}
 const OneWork = ({link, index}) => {
 
 	const [visible, setVisible] = useState(index === 0 ? true : false);
@@ -71,7 +76,6 @@ const OneWork = ({link, index}) => {
 	const seeMore = (e) => {
 	    e.preventDefault();
 	    setVisible(!visible);
-	    console.log("open/close")
 	}
 	const indexPstyle = visible ? {
 		fontFamily: 'BonVivantSerif, serif',
@@ -90,22 +94,13 @@ const OneWork = ({link, index}) => {
               }
               {visible ? 
               <div  className="oneWorkGrid"> 
-                  <div  className="imgWrap">
-                    <img
-                      src={link.image}
-                      alt={link.image} 
-                      placeholder="blurred"
-                      width= "100%" 
-                      style={imgStyle}/>
-                  </div>
-                  <div className="infoWrap">
+                  <div>
                     <h2 style={{fontFamily: 'BonVivantSerif, serif', fontSize: '2rem', marginTop: '20px'}}> {link.title} </h2>
                     <div style={desc1} > 
-
                       <div style={lineStyle}> <h3 style= {{width: '30%', fontWeight: 500, color: '#787878'}}> Role </h3> <div style= {{width: '70%'}}> {link.role} </div> </div>
                       {
                         link.url ? (
-                          <div style={lineStyle}> <h3 style= {{width: '30%', fontWeight: 500, color: '#787878'}}> URL </h3> <div style= {{width: '70%'}}> <a href={link.url} style={{textDecoration: 'none', color: 'black' }}> {link.url}</a></div> </div>
+                          <div style={lineStyle}> <h3 style= {{width: '30%', fontWeight: 500, color: '#787878'}}> URL </h3> <div style= {{width: '70%'}}> <a href={link.url} target="_blank" style={{textDecoration: 'none', color: 'black' }}> {link.url}</a></div> </div>
                         ) : ("")
                       }
                       {
@@ -120,25 +115,33 @@ const OneWork = ({link, index}) => {
                           )} </div>
                         </div> 
                       </div>
-                      
-                      
                     </div>
-                    <div style={desc2}>
-                      <h3 style= {{fontWeight: 500, color: '#787878', paddingBottom: '10px'}}> Technologies </h3> 
-                      <div style= {{}}> {link.technologies.map(tech => (
-                          <p key={tech}> {tech} </p>
-                        ))} </div> <br />
-                      <h3 style= {{fontWeight: 500, color: '#787878', paddingBottom: '10px'}}> My Contribution </h3> 
-                      <p style= {{}}> {link.contribution} </p> <br />
-                      <p style= {{fontStyle: 'italic', fontWeight: 400, marginBottom: '50px'}}> {link.italictext} </p>
-                      <button style={buttonStyle2} onClick = {(e)=>seeMore(e)}> close </button> 
-                    </div>
+                    <div>
+                      <div style ={{'display':'flex'}}>
+                      {/* {link.image !== "" ? link.image : null} */}
+                        <div style={{'paddingTop': '35px'}}>
+                          <h3 style= {{fontWeight: 500, color: '#787878', paddingBottom: '10px'}}> Technologies </h3> 
+                          <div style= {{}}> {link.technologies.map(tech => (
+                              <p key={tech}> {tech} </p>
+                            ))}
+                          </div>
+                        </div>
+                        
+                      </div>
+                      <div style={desc2}> 
+                        <h3 style= {{fontWeight: 500, color: '#787878', paddingBottom: '10px'}}> My Contribution </h3> 
+                        <p style= {{}}> {link.contribution} </p> <br />
+                        <p style= {{fontStyle: 'italic', fontWeight: 400, marginBottom: '50px'}}> {link.italictext} </p>
+                        <button style={buttonStyle2} onClick = {(e)=>seeMore(e)}> close </button> 
+                      </div>
+                      
                   </div>
-
+                  
 
               </div>
-              : <div className="oneWorkGridClosed"> 
-              
+              </div>
+              : 
+              <div className="oneWorkGridClosed"> 
               <div style={{marginTop: "10px", display: 'flex', flexWrap: 'wrap'}}> {link.tags.map(tag => 
                           <span key ={tag} style={tagStyle}> {tag} </span>
                           )} </div>
@@ -146,10 +149,8 @@ const OneWork = ({link, index}) => {
               <p> {link.description} </p>
                         
               <button style={buttonStyle} className="button" onClick = {(e)=>seeMore(e)}> see more </button> 
-              </div> }
-              
-            </div>
-		)
-	
-}
+              </div>
+              }
+</div>
+)};
 export default OneWork;
--- src/components/WorksComponent.js
+++ src/components/WorksComponent.js
@@ -1,6 +1,10 @@
 import * as React from "react"
 import '../styles/typography.css';
 import OneWork from './Onework';
+import KravePic from '../images/project_svg/krave.svg';
+import AkmPic from '../images/project_svg/akm.svg';
+import DanaPic from '../images/project_svg/dana.svg';
+import TntPic from '../images/project_svg/tnt.svg';
 // import Blog from './Blog';
 const title = {
   fontFamily: 'BonVivantSerif, serif',
@@ -22,24 +26,10 @@ const links = [
       "Beauty products company specilliasing in healthy skincare with organic products",
     code: '',
     tags: ["Client Work", "Front End"],
-    image: 'https://user-images.githubusercontent.com/47024219/179133773-c023342b-0c85-4656-bb70-17cafe75df98.png',
+    image: <KravePic width="40%" height="max-content" />,
     technologies: ["Shopify (platform)", "Liquid", "JS(ES6), HTML5, CSS3", "AWS instance", "WHOIS domain"],
     contribution: "Using client’s Figma Design files, worked on the design and functionality of specific pages and components. Integrated Shopify store with third party delivery and shopping apps. Worked on data transfer as a part of full data migration between different platforms. Created and managed AWS ec2 instance as a server for the application code. Installed SSL and deployed application code to the running server. ",
     italictext: "Navigation Bar, Registration & Login, Cart, Product Pages, Products Grid, Footer, Checkout Page.",
-  },
-  {
-    title: "T1",
-    role: "Front End Developer",
-    url: "https://shop-t1.gg/",
-    description:
-      "Cyber Sport Company’s online shopping store for selling customizable merch products. ",
-    code: '',
-    tags: ["Client Work", "Front End"],
-    image: 'https://user-images.githubusercontent.com/47024219/179134011-f3c9f9c5-b80b-436c-b5e0-0a913bb4a66c.png',
-    technologies: ["Shopify (platform)", "Liquid", "JS(ES6), HTML5, CSS3"],
-    contribution: "Modified and customized components and pages of the store. Worked on adding customization options to products such as adding customer’s name on T-shirt and displaying such changes on the product page. ",
-    italictext: "Product Pages, Products Grid, Custom Pop-ups, Cart Page.",
-    
   },
   {
     title: "thisisneverthat",
@@ -49,24 +39,24 @@ const links = [
       "Contemporary fashion brand’s online store.",
     code: '',
     tags: ["Client Work", "Back End"],
-    image: 'https://user-images.githubusercontent.com/47024219/179134105-e3131887-a9f5-4e0a-aaeb-03e663e48b98.png',
+    image: <TntPic width="40%" height="max-content" />,
     technologies: ["Shopify (platform)", "Liquid", "JS(ES6), HTML5, CSS3", "Kakao API", "Naver API"],
     contribution: "Worked on integration of the third party payment systems (Kakao and Naver Pay) into the Shopify-based store.",
     italictext: "Checkout Page, Cart Page",
     
   },
-   {
-    title: "L’atelier des Parfums",
-    role: "Front End Developer",
-    url: "https://ldp.co.kr/",
+  {
+    title: "Landing Page for a Course",
+    role: "Designer, Developer",
+    url: "http://nysana.tilda.ws/arman/",
     description:
-      "Luxury perfum online store.",
+      "Landing page for a course registration with embedded form submission",
     code: '',
-    tags: ["Client Work", "Front End"],
-    image: 'https://user-images.githubusercontent.com/47024219/179134243-70af7bfe-7fa3-4a08-adb5-61bb2c4bda62.png',
-    technologies: ["Shopify (platform)", "Liquid", "JS(ES6), HTML5, CSS3"],
-    contribution: "Modified and customized components and pages of the store. Worked on data storage of the customers upon registration to the website.",
-    italictext: "Registration Page, Products Page, Checkout Page, Cart Page. ",
+    tags: ["Freelance", "UI UX Design", "CMS"],
+    image: <DanaPic width="40%" height="max-content" />,
+    technologies: ["Figma", "Tilda CMS (platform)", "HTML5, CSS3"],
+    contribution: "Worked on both design and development of the landing page, using CMS of client's preference - Tilda CMS. Used SVG images in order to create unique designs given the limitations of CMS platform.",
+    italictext: "Design on Figma, full CMS development, and deployment.",
     
   },
    {
@@ -77,10 +67,10 @@ const links = [
     description:
       "Personal Portfolio of an aspiring photographer.",
     tags: ["Freelance", "Front End", "UI UX Design"],
-    image: 'https://user-images.githubusercontent.com/47024219/179134379-e117aef2-d988-4d85-83f1-cde9d9f81550.png',
+    image: <AkmPic width="40%" height="max-content" />,
     technologies: ['GatsbyJS', 'Figma', 'JS(ES6), HTML5, CSS3'],
     contribution: "Prepared UI UX design in Figma and coded static personal portfolio website for a client. Client is a photographer, so speed of loading images was crucial. For optimal speed, Gatsby JS was used as a framework and deployment.",
-    italictext: "Designed, coded and deployed.",
+    italictext: "Design on Figma, full Front-End development, and deployment.",
     
   },
   {
@@ -91,52 +81,10 @@ const links = [
       "Automating the collection of Naver News articles with keywords in Google Sheets using Python.",
     code: 'https://github.com/punchkorea/Jupyter-to-GoogleSheets',
     tags: ["Freelance", "Front End", "UI UX Design"],
-    image: 'https://user-images.githubusercontent.com/47024219/179463483-82aea0b9-9722-4192-8fc5-2861e1500e31.png',
+    image: '',
     technologies: ['Python', 'Libraries (Pandas, BeautifulSoup)', 'Google Drive API', 'Goole Spreadsheets API'],
     contribution: "Worked on automation of collection of news articles from a website and storing them in easy-to-read way for the marketing team.",
-    italictext: "Coded and installed on live server for daily run.",
-    
-  },
-  {
-    title: "Landing Page for a Course",
-    role: "Designer",
-    url: '',
-    description:
-      "Landing page for life coach and her course. ",
-    code: '',
-    tags: ["Freelance", "UI UX Design"],
-    technologies: ['Figma'],
-    image: 'https://user-images.githubusercontent.com/47024219/179134567-d96d97b0-ce43-4017-892e-588598ab4db4.png',
-    contribution: "Designed landing page for a life coach’s course. Based on course content and potential audience, designed easy-to-read website frames.",
-    italictext: "Provided full design on Figma",
-    
-  },
-  {
-    title: "Portfolio for a Developer",
-    role: "Designer",
-    url: '',
-    description:
-      "Personal portfolio for a back-end developer.",
-    code: '',
-    tags: ["Freelance", "UI UX Design"],
-    technologies: ['Figma'],
-    image: 'https://user-images.githubusercontent.com/47024219/179134724-a3b63913-de93-4f4c-9d84-32ba3ca61083.png',
-    contribution: "Designed personal portfolio for a back-end developer, focusing on highligthing developer's skills.",
-    italictext: "Provided full design on Figma",
-    
-  },
-  {
-    title: "Try Find Face",
-    role: "Full Stack Developer",
-    url: 'https://try-find-face.herokuapp.com/',
-    description:
-      "Application which is built to implement AI Face Detection service into a simple application.",
-      code: 'https://github.com/anapimolodec/try-find-face',
-    tags: ["Project", "Full Stack"],
-    image: 'https://github.com/anapimolodec/portfolio/blob/main/src/components/images/face.png?raw=true',
-    technologies: ['React.js', 'Node.js', 'Express', 'Clarifai (API)', 'Tachyons(Toolkit)', 'JS(ES6), HTML5, CSS3','Heroku'],
-    contribution: "A user can register and upload an Image Url Address to detect face or multiple faces on the provided image. The app counts how many times the user detected faces and allows user to sign in any time later and continue using the app. The face detection algorithm is provided by Clarifai REST API.",
-    italictext: "Full Front-End, Back-End development, database development, design, and connection to REST API.",
+    italictext: "Coding and installation on live server for daily run.",
     
   },
   {
@@ -147,10 +95,10 @@ const links = [
       "My personal portfolio. 2022 Updated version.",
     code: 'https://github.com/anapimolodec/portfolio2022',
     tags: ["Project", "Front End"],
-    image: 'https://user-images.githubusercontent.com/47024219/179464372-1e099798-3b79-43a9-a94c-7a13977320ec.jpg',
+    image: '',
     technologies: ['Gatsby JS', 'EmailJS', 'graphql', 'JS(ES6), HTML5, CSS3', 'Figma', 'Netlify'],
-    contribution: "Static Website which is a brief summary of my web development journey.",
-    italictext: "Full Front-End development, Figma design, and deployment.",
+    contribution: "Static Website which is a brief summary of my web development journey connected to EmailJS and Google Analytics to track visitors.",
+    italictext: "Design on Figma, full Front-End development, and deployment.",
     
   },
 
@@ -175,3 +123,76 @@ const WorksComponent = () => {
 }
 
 export default WorksComponent;
+
+/* OLD 
+{
+  title: "T1",
+  role: "Front End Developer",
+  url: "https://shop-t1.gg/",
+  description:
+    "Cyber Sport Company’s online shopping store for selling customizable merch products. ",
+  code: '',
+  tags: ["Client Work", "Front End"],
+  image: 'https://user-images.githubusercontent.com/47024219/179134011-f3c9f9c5-b80b-436c-b5e0-0a913bb4a66c.png',
+  technologies: ["Shopify (platform)", "Liquid", "JS(ES6), HTML5, CSS3"],
+  contribution: "Modified and customized components and pages of the store. Worked on adding customization options to products such as adding customer’s name on T-shirt and displaying such changes on the product page. ",
+  italictext: "Product Pages, Products Grid, Custom Pop-ups, Cart Page.",
+  
+},
+{
+  title: "L’atelier des Parfums",
+  role: "Front End Developer",
+  url: "https://ldp.co.kr/",
+  description:
+    "Luxury perfum online store.",
+  code: '',
+  tags: ["Client Work", "Front End"],
+  image: 'https://user-images.githubusercontent.com/47024219/179134243-70af7bfe-7fa3-4a08-adb5-61bb2c4bda62.png',
+  technologies: ["Shopify (platform)", "Liquid", "JS(ES6), HTML5, CSS3"],
+  contribution: "Modified and customized components and pages of the store. Worked on data storage of the customers upon registration to the website.",
+  italictext: "Registration Page, Products Page, Checkout Page, Cart Page. ",
+  
+},
+{
+  title: "Landing Page for a Course",
+  role: "Designer",
+  url: '',
+  description:
+    "Landing page for life coach and her course. ",
+  code: '',
+  tags: ["Freelance", "UI UX Design"],
+  technologies: ['Figma'],
+  image: 'https://user-images.githubusercontent.com/47024219/179134567-d96d97b0-ce43-4017-892e-588598ab4db4.png',
+  contribution: "Designed landing page for a life coach’s course. Based on course content and potential audience, designed easy-to-read website frames.",
+  italictext: "Provided full design on Figma",
+  
+},
+  {
+    title: "Portfolio for a Developer",
+    role: "Designer",
+    url: '',
+    description:
+      "Personal portfolio for a back-end developer.",
+    code: '',
+    tags: ["Freelance", "UI UX Design"],
+    technologies: ['Figma'],
+    image: 'https://user-images.githubusercontent.com/47024219/179134724-a3b63913-de93-4f4c-9d84-32ba3ca61083.png',
+    contribution: "Designed personal portfolio for a back-end developer, focusing on highligthing developer's skills.",
+    italictext: "Provided full design on Figma",
+    
+  },
+   {
+    title: "Try Find Face",
+    role: "Full Stack Developer",
+    url: 'https://try-find-face.herokuapp.com/',
+    description:
+      "Application which is built to implement AI Face Detection service into a simple application.",
+      code: 'https://github.com/anapimolodec/try-find-face',
+    tags: ["Project", "Full Stack"],
+    image: 'https://github.com/anapimolodec/portfolio/blob/main/src/components/images/face.png?raw=true',
+    technologies: ['React.js', 'Node.js', 'Express', 'Clarifai (API)', 'Tachyons(Toolkit)', 'JS(ES6), HTML5, CSS3','Heroku'],
+    contribution: "A user can register and upload an Image Url Address to detect face or multiple faces on the provided image. The app counts how many times the user detected faces and allows user to sign in any time later and continue using the app. The face detection algorithm is provided by Clarifai REST API.",
+    italictext: "Full Front-End, Back-End development, database development, design, and connection to REST API.",
+    
+  },
+*/
